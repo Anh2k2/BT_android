bd6d3122e1e9876a90f66202bf5016a8bca9df25
Quynh Anh <nguyenquynhanh9a3@gmail.com>
2023-02-24 07:11:04 +0700

    First

---
+++ app/src/main/java/com/example/bt/ContactAdapter.java
@@ -0,0 +1,118 @@
+package com.example.bt;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class ContactAdapter extends BaseAdapter {
+
+    private Context context;
+    private int layout;
+    private List<Contact> contactList;
+
+    public ContactAdapter(Context context, int layout, List<Contact> contactList) {
+        this.context = context;
+        this.layout = layout;
+        this.contactList = contactList;
+    }
+
+    @Override
+    public int getCount() {
+        return contactList.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        ViewHolder viewHolder;
+
+        if (view == null){
+            viewHolder = new ViewHolder();
+            LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = layoutInflater.inflate(layout, null);
+
+            viewHolder.tv_name = view.findViewById(R.id.tv_name);
+            viewHolder.tv_phone = view.findViewById(R.id.tv_phone);
+            viewHolder.cb_status = view.findViewById(R.id.cb_status);
+            viewHolder.img_call = view.findViewById(R.id.img_call);
+            viewHolder.img_send = view.findViewById(R.id.img_sms);
+            view.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) view.getTag();
+        }
+
+        Contact c = contactList.get(i);
+
+        viewHolder.tv_name.setText(c.getName());
+        viewHolder.tv_phone.setText(c.getPhone());
+        if (c.getStatus()){
+            viewHolder.cb_status.setChecked(true);
+        } else {
+            viewHolder.cb_status.setChecked(false);
+        }
+
+        viewHolder.img_call.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentCall = new Intent();
+                intentCall.setAction(Intent.ACTION_DIAL);
+                intentCall.setData(Uri.parse("tel:" + c.getPhone()));
+                context.startActivity(intentCall);
+            }
+        });
+
+        viewHolder.img_send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentSend = new Intent();
+                intentSend.setAction(Intent.ACTION_SENDTO);
+                intentSend.setData(Uri.parse("sms:" + c.getPhone()));
+                context.startActivity(intentSend);
+            }
+        });
+
+        viewHolder.cb_status.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b){
+                    c.setStatus(true);
+                    viewHolder.cb_status.setChecked(c.getStatus());
+
+                } else {
+                    c.setStatus(false);
+                    viewHolder.cb_status.setChecked(c.getStatus());
+
+                }
+            }
+        });
+
+        return view;
+    }
+
+    private class ViewHolder{
+        TextView tv_name, tv_phone;
+        CheckBox cb_status;
+        ImageView img_call, img_send;
+    }
+}
---
+++ app/src/main/java/com/example/bt/MainActivity.java
@@ -0,0 +1,101 @@
+package com.example.bt;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    SearchView searchView;
+    ListView listView;
+    Button button_add, button_delete;
+    ArrayList<Contact> contacts;
+    ContactAdapter contactAdapter;
+
+    private void init(){
+        searchView = findViewById(R.id.search_item);
+        listView = findViewById(R.id.lv_contact);
+        button_add = findViewById(R.id.btn_add);
+        button_delete = findViewById(R.id.btn_del);
+        contacts = new ArrayList<>();
+        contacts.add(new Contact(1, "Nguyen Van An", "0337693148", true));
+        contacts.add(new Contact(2, "Tran Van Ba", "0337801993", true));
+        contacts.add(new Contact(3, "Hoang Thuy Dung", "0337793145", false));
+        contacts.add(new Contact(4, "Dinh Thi Tuyen", "0339693148", true));
+        contacts.add(new Contact(5, "Dang Van Hien", "0337693479", true));
+        contactAdapter = new ContactAdapter(MainActivity.this, R.layout.userlayout, contacts);
+        listView.setAdapter(contactAdapter);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        getSupportActionBar().hide();
+        init();
+
+        Intent intent = getIntent();
+        if (intent != null){
+            Bundle bundle = intent.getBundleExtra("bundle");
+            if (bundle != null){
+                String n = bundle.getString("name");
+                String p = bundle.getString("phone");
+                contacts.add(new Contact(0, n, p, false));
+                contactAdapter.notifyDataSetChanged();
+            }
+
+        }
+
+        button_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, AddContact.class));
+            }
+        });
+
+        button_delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Notification");
+                builder.setMessage("Are you sure?");
+                builder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int pos) {
+                        for (int i = 0; i < contacts.size(); i++){
+                            if (contacts.get(i).getStatus()){
+                                contacts.remove(i);
+                                contactAdapter.notifyDataSetChanged();
+                            }
+                        }
+                    }
+                });
+                builder.setNeutralButton("CANCEL", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+                builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+                builder.show();
+
+            }
+        });
+    }
+}
